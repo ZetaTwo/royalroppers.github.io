5fb00573b507a8eb2d8144985866863079a68932
Calle Svensson <calle.svensson@zeta-two.com>
2025-04-05 22:49:53 +0200

    Improve semantics of team description

--- src/Components/about.js
+++ src/Components/about.js
@@ -1,20 +1,16 @@
 const About = function () {
     return (
         <div className="pt-2 px-3 flex flex-col">
-            <div className="default-text text-justify m-auto text-xl max-w-3xl p-2">
+            <p className="default-text text-justify m-auto text-xl max-w-3xl p-2">
                 RoyalRoppers is a Swedish student CTF team. We regularly play
                 CTFs of all difficulty levels and have members ranging from
                 absolute beginners to very experienced players.
-                The majority of us live in Stockholm and we sometimes meet up to play CTFs onsite at KTH.&nbsp;
-                <a className="text-[#f9c867]" href="https://ctftime.org/team/176008">Here</a> is our CTF-time.
-            </div>
-            <h2 className="text-center pt-3 pb-3 default-text text-3xl">
-                Want to play CTFs with us? Click&nbsp;
-                <a className="text-[#f9c867] underline underline-offset-2"
-                    href="https://forms.gle/uVaYpFLjF7a6JGGC7">here
-                </a>
-                &nbsp;to join!
-            </h2>
+                The majority of us live in Stockholm and we sometimes meet up to play CTFs onsite at KTH.
+                Check out our ranking and past results on <a className="text-[#f9c867]" href="https://ctftime.org/team/176008">our CTFTime page</a>.
+            </p>
+            <p className="text-center pt-3 pb-3 default-text text-2xl">
+                Want to play CTFs with us? <a className="text-[#f9c867] underline underline-offset-2" href="https://forms.gle/uVaYpFLjF7a6JGGC7">Submit an application</a>
+            </p>
         </div>
     );
 };
